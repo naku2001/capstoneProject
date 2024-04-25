a9177bdaf2af605c20cd5c3e3acf12442ba2be11
perfect.makuwerere@students.uz.ac.zw <angelsmakuwerere@gmail.com>
2024-04-25 19:34:10 +0200

    initial commit

---
+++ Main.py
@@ -0,0 +1,78 @@
+import pickle
+from pathlib import Path
+
+import pandas as pd  # pip install pandas openpyxl
+
+import streamlit as st  # pip install streamlit
+import streamlit_authenticator as stauth  # pip install streamlit-authenticator
+
+
+# emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
+st.set_page_config(page_title="Text Extraction 📄", layout="wide")
+
+hide_bar= """
+    <style>
+    [data-testid="stSidebar"][aria-expanded="true"] > div:first-child {
+        visibility:hidden;
+        width: 0px;
+    }
+    [data-testid="stSidebar"][aria-expanded="false"] > div:first-child {
+        visibility:hidden;
+    }
+    </style>
+"""
+
+# --- USER AUTHENTICATION ---
+names = ["lucy", "lucy","admin"]
+usernames = ["lucy", "lucy","admin"]
+
+# load hashed passwords
+file_path = Path(__file__).parent / "hashed_pw.pkl"
+with file_path.open("rb") as file:
+    hashed_passwords = pickle.load(file)
+
+authenticator = stauth.Authenticate(names, usernames, hashed_passwords,
+    "SIPL_dashboard", "abcdef")
+
+name, authentication_status, username = authenticator.login("Login", "main")
+
+if authentication_status == False:
+    st.error("Username/password is incorrect")
+    st.markdown(hide_bar, unsafe_allow_html=True)
+
+if authentication_status == None:
+    st.warning("Please enter your username and password")
+    st.markdown(hide_bar, unsafe_allow_html=True)
+
+
+if authentication_status:
+    # # ---- SIDEBAR ----
+   
+    # st.sidebar.header("select page here :")
+    
+
+    st.title('🔠 Text exctraction')
+    st.header('🔍 Extract text from images')
+    st.subheader('Export Text to favourable format 📄')
+    st.write('Check it out now! 🕵️‍♀️🔍')
+
+
+   
+
+
+   
+
+    st.sidebar.success("Select a page above.")
+
+    ###---- HIDE STREAMLIT STYLE ----
+    hide_st_style = """
+                <style>
+                #MainMenu {visibility: hidden;}
+                footer {visibility: hidden;}
+                header {visibility: hidden;}
+                </style>
+                """
+    st.markdown(hide_st_style, unsafe_allow_html=True)
+
+
+    authenticator.logout("Logout", "sidebar")
---
+++ functions.py
@@ -0,0 +1,164 @@
+import streamlit as st
+from zipfile import ZipFile
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfpage import PDFPage
+from io import StringIO
+import streamlit_authenticator as stauth
+import base64
+import yaml
+from yaml.loader import SafeLoader
+import re
+#------- OCR ------------
+import pdf2image
+import pytesseract
+from pytesseract import Output, TesseractError
+import os
+# GOOGLE_APPLICATION_CREDENTIALS = "capstone_project/marine-access-418412-e586b735cf01.json"
+# import os
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "marine-access-418412-e586b735cf01.json"
+
+@st.cache_data
+def images_to_txt(path, language):
+    images = pdf2image.convert_from_bytes(path)
+    all_text = []
+    for i in images:
+        pil_im = i
+        text = pytesseract.image_to_string(pil_im, lang=language)
+        # ocr_dict = pytesseract.image_to_data(pil_im, lang='eng', output_type=Output.DICT)
+        # ocr_dict now holds all the OCR info including text and location on the image
+        # text = " ".join(ocr_dict['text'])
+        # text = re.sub('[ ]{2,}', '\n', text)
+        all_text.append(text)
+    return all_text, len(all_text)
+
+# @st.cache_data   
+# def detect_handwriting(path):
+#     """Detects handwriting in the file."""
+#     client = vision.ImageAnnotatorClient()
+
+#     with open(path, 'rb') as image_file:
+#         content = image_file.read()
+
+#     image = vision.Image(content=content)
+#     response = client.document_text_detection(image=image)  # Use document_text_detection for handwriting
+#     texts = response.text_annotations
+#     all_text = []
+
+#     print('Detected Text:')
+
+#     for text in texts:
+#       print('\n{}'.format(text.description))
+        
+#     all_text.append(text)
+
+#     if response.error.message:
+#         raise Exception('{}\nFor more info on error messages, check: https://cloud.google.com/apis/design/errors'.format(response.error.message))
+
+#     return all_text
+
+   
+
+@st.cache_data
+def convert_pdf_to_txt_pages(path):
+    texts = []
+    rsrcmgr = PDFResourceManager()
+    retstr = StringIO()
+    laparams = LAParams()
+    device = TextConverter(rsrcmgr, retstr, laparams=laparams)
+    # fp = open(path, 'rb')
+    interpreter = PDFPageInterpreter(rsrcmgr, device)
+    size = 0
+    c = 0
+    file_pages = PDFPage.get_pages(path)
+    nbPages = len(list(file_pages))
+    for page in PDFPage.get_pages(path):
+      interpreter.process_page(page)
+      t = retstr.getvalue()
+      if c == 0:
+        texts.append(t)
+      else:
+        texts.append(t[size:])
+      c = c+1
+      size = len(t)
+    # text = retstr.getvalue()
+
+    # fp.close()
+    device.close()
+    retstr.close()
+    return texts, nbPages
+
+@st.cache_data
+def convert_pdf_to_txt_file(path):
+    texts = []
+    rsrcmgr = PDFResourceManager()
+    retstr = StringIO()
+    laparams = LAParams()
+    device = TextConverter(rsrcmgr, retstr, laparams=laparams)
+    # fp = open(path, 'rb')
+    interpreter = PDFPageInterpreter(rsrcmgr, device)
+    
+    file_pages = PDFPage.get_pages(path)
+    nbPages = len(list(file_pages))
+    for page in PDFPage.get_pages(path):
+      interpreter.process_page(page)
+      t = retstr.getvalue()
+    # text = retstr.getvalue()
+
+    # fp.close()
+    device.close()
+    retstr.close()
+    return t, nbPages
+
+@st.cache_data
+def save_pages(pages):
+  
+  files = []
+  for page in range(len(pages)):
+    filename = "page_"+str(page)+".txt"
+    with open("./file_pages/"+filename, 'w', encoding="utf-8") as file:
+      file.write(pages[page])
+      files.append(file.name)
+  
+  # create zipfile object
+  zipPath = './file_pages/pdf_to_txt.zip'
+  zipObj = ZipFile(zipPath, 'w')
+  for f in files:
+    zipObj.write(f)
+  zipObj.close()
+
+  return zipPath
+
+def login():
+    
+   
+    with open('cred.yaml') as file:
+         config = yaml.load(file, Loader=SafeLoader)
+    authenticator = stauth.Authenticate(
+            config['credentials'],
+            config['cookie']['name'],
+            config['cookie']['key'],
+            config['cookie']['expiry_days'],
+            config['preauthorized']
+        )
+    global name, authentication_status, username 
+    name, authentication_status, username = authenticator.login('main')
+    if authentication_status:
+       pass
+    elif authentication_status == False:
+        st.error('Username/password is incorrect')
+    elif authentication_status == None:
+        st.warning('Please enter your username and password')
+
+
+
+def displayPDF(file):
+  # Opening file from file path
+  # with open(file, "rb") as f:
+  base64_pdf = base64.b64encode(file).decode('utf-8')
+
+  # Embedding PDF in HTML
+  pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf"></iframe>'
+  # Displaying File
+  st.markdown(pdf_display, unsafe_allow_html=True)
